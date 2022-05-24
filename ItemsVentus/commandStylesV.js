2f5e8c96cb7e62769a40e80b7e6a757bb4c71ff8
dewillm <DenisWillm@t-online.de>
2022-05-25 01:59:54 +0200

    Adds Command Styles for Ventus into the pool

---
+++ ItemsVentus/commandStylesV.js
@@ -0,0 +1,11 @@
+export const commandStylesV = [
+  "010154", // Fever Pitch
+  "010155", // Firestorm
+  "010156", // Diamond Dust
+  "010157", // Thunderbolt
+  "010158", // Cyclone
+  "01015A", // Sky Climber
+  "01015D", // Rhythm Mixer
+  "01015F", // Wingblade
+  "010160", // Frozen Fortune
+];
