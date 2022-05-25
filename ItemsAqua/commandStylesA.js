ff4247323aed2ff0da63f755713190d19db2416e
dewillm <DenisWillm@t-online.de>
2022-05-25 21:07:21 +0200

    Adds D-Links and Command Styles to Aqua

---
+++ ItemsAqua/commandStylesA.js
@@ -0,0 +1,11 @@
+export const commandStylesA = [
+  "010153", // Spellweaver
+  "010155", // Firestorm
+  "010156", // Diamond Dust
+  "010157", // Thunderbolt
+  "010159", // Bladecharge
+  "01015A", // Sky Climber
+  "01015C", // Ghost Drive
+  "01015D", // Rhythm Mixer
+  "010160", // Frozen Fortune
+];
