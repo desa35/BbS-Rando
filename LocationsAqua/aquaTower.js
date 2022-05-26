b43b5e9c5c260efd0949eef24eff4e46ef89e6f2
dewillm <DenisWillm@t-online.de>
2022-05-26 14:57:42 +0200

    Fixes the formatting within the arrays

--- LocationsAqua/aquaTower.js
+++ LocationsAqua/aquaTower.js
@@ -1,6 +1,6 @@
 export const aquaTower = [
-    "D73166", // Magnega
-    "D7316E", // Shimmering Crystal
-    "D73176", // Megalixir
-    "D7317E", // Mega Attack Recipe
+  "D73166", // Magnega
+  "D7316E", // Shimmering Crystal
+  "D73176", // Megalixir
+  "D7317E", // Mega Attack Recipe
 ];
--- LocationsTerra/terraOlympus.js
+++ LocationsTerra/terraOlympus.js
@@ -1,6 +1,6 @@
 export const terraOlympus = [
-    "D72C2E", // Fire Strike
-    "D72C36", // Mega Attack Recipe
-    "D72C3E", // Mega-Potion
-    "D72C46", // Map (Olympus Coliseum)
+  "D72C2E", // Fire Strike
+  "D72C36", // Mega Attack Recipe
+  "D72C3E", // Mega-Potion
+  "D72C46", // Map (Olympus Coliseum)
 ];
