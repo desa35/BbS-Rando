7b449af99a2f9a27833439edc41ae6ca6158d409
dewillm <DenisWillm@t-online.de>
2022-05-29 22:30:03 +0200

    Adds Battle Level Randomization

--- RandomBattlelevel/index.js
+++ RandomBattlelevel/index.js
@@ -1,5 +1,5 @@
-// Initializes variable for the lua offset that needs to be subtracted
-const offset = parseInt("60E334", 16);
+import { orbsCheck } from "../RandoInitialization/index.js";
+import { offset } from "../RandoGeneralFunctions/index.js";
 
 // Initializes variable for the Battle Level
 const battleLevel = parseInt("10F9F505", 16) - offset;
@@ -19,9 +19,9 @@ export function randomBattleLevelGenerator(min, max) {
 }
 
 // Initializes function to write Battle Level randomization (Hardcore)
-export function randomBattleLevel() {
-  let luascript = [];
-  for (let i = 0; i < 12; i++) {
+export function randomBattleLevel(characterDigit) {
+  let luascript = [orbsCheck[characterDigit.value].value];
+  for (let i = 1; i < 12; i++) {
     let random = randomBattleLevelGenerator(4, 1);
     let level = battleLevelAddress(i);
     luascript.push(`WriteByte(0x${level}, 0x${random})`);
@@ -29,5 +29,6 @@ export function randomBattleLevel() {
   let random = randomBattleLevelGenerator(10, 8);
   let level = battleLevelAddress(12);
   luascript.push(`WriteByte(0x${level}, 0x${random})`);
+  luascript.push(`end`);
   return luascript.join("\n");
 }
