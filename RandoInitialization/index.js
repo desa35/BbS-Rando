e6c898a8fb148227bc47ba5865505e90eaf51376
dewillm <DenisWillm@t-online.de>
2022-05-29 16:52:00 +0200

    Moves Orb Fight Check to Initialization file

--- RandoInitialization/index.js
+++ RandoInitialization/index.js
@@ -4,6 +4,22 @@ const offset = parseInt("60E334", 16);
 // Initializes variable for all 3 Stories' for character checks later on (Offset already subtracted)
 const storyModifier = parseInt("10F9CD00", 16) - offset;
 
+// Initializes array for the Orb Battle conditional for Ventus, Terra and Aqua
+export const orbsCheck = [
+  {
+    character: "Ventus",
+    value: `if ReadInt(0x206D6B) == 0x20100 and ReadInt(0x206D6F) == 0x44004400 and ReadShort(0x206D73) == 0x4400 then`,
+  },
+  {
+    character: "Aqua",
+    value: `if ReadInt(0x206D6B) == 0x20100 and ReadInt(0x206D6F) == 0x4B004B00 and ReadShort(0x206D73) == 0x4B00 then`,
+  },
+  {
+    character: "Terra",
+    value: `if ReadInt(0x206D6B) == 0x20100 and ReadInt(0x206D6F) == 0x3B003B00 and ReadShort(0x206D73) == 0x3B00 then`,
+  },
+];
+
 // Initializes function that write the checks for which character is being used
 export function storyCheck(characterDigit) {
   const characterCheck = `if ReadInt(0x206D6B) ~= 0xFFFFFF00 then
