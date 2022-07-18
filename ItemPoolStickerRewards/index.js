eead3c70a3ac5f6fda1cb9f0a2bb54cade75f295
SelfRaisin <109408444+SelfRaisin@users.noreply.github.com>
2022-07-18 14:17:32 +1000

    Update index.js

--- ItemPoolStickerRewards/index.js
+++ ItemPoolStickerRewards/index.js
@@ -1,30 +1,23 @@
 import { synthCrystals } from "../ItemsGlobal/synthCrystals.js";
-import { synthOreItems } from "../ItemsGlobal/synthOreItems.js";
 
 import { stickers } from "../Stickers/index.js";
 
 const synthCrystalsVentus = synthCrystals.slice();
-const synthOreItemsVentus = synthOreItems.slice();
 const synthCrystalsAqua = synthCrystals.slice();
-const synthOreItemsAqua = synthOreItems.slice();
 const synthCrystalsTerra = synthCrystals.slice();
-const synthOreItemsTerra = synthOreItems.slice();
 
 export const stickerRewardsVentus = [
   ...synthCrystalsVentus,
-  ...synthOreItemsVentus,
   ...stickers[0],
 ];
 
 export const stickerRewardsAqua = [
   ...synthCrystalsAqua,
-  ...synthOreItemsAqua,
   ...stickers[1],
 ];
 
 export const stickerRewardsTerra = [
   ...synthCrystalsTerra,
-  ...synthOreItemsTerra,
   ...stickers[2],
 ];
 
